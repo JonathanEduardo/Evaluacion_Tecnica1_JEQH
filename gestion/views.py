75ddda45603a69fe882382d90b8ea8453e0e8013
Jonathan <cristiano2995@gmail.com>
2024-09-20 00:41:39 -0600

    Views, Creacion EditorView y LibroView

--- gestion/views.py
+++ gestion/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .models import Autor, Libro # IMPORT MY MODELS
-from .serializers import AutorSerializer, LibroSerializer #IMPORT SERIALIZERS 
-from rest_framework.exceptions import ValidationError
+from .models import Autor, Libro # import modelos
+from .serializers import AutorSerializer, LibroSerializer #Import serlializers 
+from rest_framework.exceptions import ValidationError # Import validation error
+
 
 # Create your views here.
 
-
+# ViewSet for the Autor model
 class AutorViewSet(viewsets.ModelViewSet):
-    queryset = Autor.objects.all()
-    serializer_class = AutorSerializer
+    queryset = Autor.objects.all()  # Get all Autor objects
+    serializer_class = AutorSerializer  # Use AutorSerializer for serialization
 
-    # Validación para evitar eliminar autores con libros asociados
+ # Validation to prevent deleting authors with associated books
     def perform_destroy(self, instance):
-        if instance.libro_set.exists():
+        if instance.libro_set.exists(): # Check if the author has associated books
             raise ValidationError("No se puede eliminar un autor con libros asociados.")
-        instance.delete()
+        instance.delete() # Delete the author if no associated books
 
+# ViewSet for the Libro model
 class LibroViewSet(viewsets.ModelViewSet):
-    queryset = Libro.objects.all()
-    serializer_class = LibroSerializer
+    queryset = Libro.objects.all() # Get all Libro objects
+    serializer_class = LibroSerializer # Use LibroSerializer for serialization
