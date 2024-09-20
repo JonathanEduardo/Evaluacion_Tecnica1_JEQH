8089f4be5e8cb3c0012cd5f45d2d73b699ef5fde
Jonathan <cristiano2995@gmail.com>
2024-09-19 23:57:19 -0600

    Configuración basica inicial proyecto Django: Modelos, Migraciones, config Base datos

---
+++ gestion/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    fecha_nacimiento = models.DateField()
+
+    def __str__(self):
+        return f"{self.nombre} {self.apellido}"
+
+class Libro(models.Model):
+    titulo = models.CharField(max_length=200)
+    fecha_publicacion = models.DateField()
+    autor = models.ForeignKey(Autor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
